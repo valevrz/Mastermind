f326e9a8259f4e491c350285e021699b638e2c22
Valeria Virzi <valeria.virzi@volkswagen.de>
2023-04-20 15:47:27 +0200

    added a submit button, that locks the current row

--- Mastermind/Mastermind/Views/GameboardView.swift
+++ Mastermind/Mastermind/Views/GameboardView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct GameboardView: View {
     @ObservedObject var viewModel: MastermindViewModel //um die Spiellogik auszuführen
+
+    @State private var isRowLocked = false //wird true, wenn der Submit-Button angeklickt wird
+    @State private var lockedRows = 10
     
     //speichern den ausgewählten Kreis und seine Position
     @State private var selectedColor = Color.red
@@ -27,35 +30,70 @@ struct GameboardView: View {
                         //speichert die Position und die Farbe vom Kreis beim antippen
                         GrayCircleView(color: color)
                             .onTapGesture {
-                                selectedRowIndex = rowIndex
-                                selectedColumnIndex = columnIndex
+                                if !isRowLocked {
+                                    selectedRowIndex = rowIndex
+                                    selectedColumnIndex = columnIndex
+                                }
                             }
                     }
                 }
                 .onAppear {
                     selectedColumnIndex = 0
-                }
-                .onChange(of: selectedRowIndex) { value in
-                    selectedColumnIndex = 0
+                    isRowLocked = false //entsperrt die aktuelle Reihe
                 }
             }
             //zeigt die Farben an, aus denen man auswählen kann
-            HStack(spacing: 20) {
+            HStack(spacing:10) {
                 ForEach(viewModel.colors, id: \.self) { color in
                     Button(action: {
                         //wenn man eine Farbe auswählt, wird sie dem Kreis an der ausgewählten Position zugewiesen
                         viewModel.selectColor(color, forRow: selectedRowIndex, column: selectedColumnIndex)
-                        selectedColumnIndex += 1 //dadurch muss man den nächsten Kreis nicht anklicken, um ihn einzufärben, wählt man eine Farbe wird automatisch der Kreis daneben eingefärbt
+
+                        //dadurch muss man den nächsten Kreis nicht anklicken, um ihn einzufärben, wählt man eine Farbe wird automatisch der Kreis daneben eingefärbt
+                        selectedColumnIndex += 1
                     }) {
                         Circle()
                             .fill(color)
                             .frame(width: 35, height: 35)
                             .overlay(Circle().stroke(Color.black, lineWidth: 2))
                     }
-                    .disabled(selectedRowIndex <= 10)
+                    .disabled(selectedRowIndex <= lockedRows || isRowLocked) //sperrt die Reihen 1-11
                 }
             }
-            .padding(.top, 20)
+            .padding(.top, 10)
+
+            //zeigt den Submit-Button und den New game Button
+            HStack {
+            Button(action: {
+                viewModel.newGame()
+                isRowLocked = false
+            }) {
+                Text("New Game")
+                }
+                .font(.title3)
+                .bold()
+                .foregroundColor(.white)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 20)
+                .background(Color.purple)
+                .cornerRadius(10)
+                .padding(.top, 10)
+
+            Button(action: {
+                isRowLocked = true //sperrt die aktuelle Reihe
+                lockedRows -= 1
+            }) {
+                Text("Submit")
+                    .font(.title3)
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 20)
+                    .background(Color.purple)
+                    .cornerRadius(10)
+                }
+                .padding(.top, 10)
+            }
         }
         .padding()
     }
