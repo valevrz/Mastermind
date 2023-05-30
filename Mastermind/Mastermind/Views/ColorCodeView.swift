2e86eccfa1d410a893c279595e454905ba14964b
Valeria Virzi <valeria.virzi@volkswagen.de>
2023-05-30 11:45:41 +0200

    Add corrections

--- Mastermind/Mastermind/Views/ColorCodeView.swift
+++ Mastermind/Mastermind/Views/ColorCodeView.swift
@@ -16,25 +16,21 @@ struct ColorCodeView: View {
 
     var body: some View {
         let color: [Color] = isGameOver ? colorCode : Array(repeating: .gray, count: viewModel.columnCount)
-        VStack {
             // Display only one row
-            ForEach(0..<1) { _ in
-                HStack {
-                    // Display 4 colored circles
-                    ForEach(0..<viewModel.columnCount) { columnIndex in
-                        if columnIndex < colorCode.count { // check if the current index is valid
-                            Circle()
-                                .foregroundColor(color[columnIndex])
-                                .overlay(Circle().stroke(Color.black, lineWidth: 2))
-                                .frame(width: 40, height: 40)
-                        }else{
-                            // Display a gray circle if there is no color in the array
-                            GrayCircleView()
-                        }
+            HStack {
+                // Display 4 colored circles
+                ForEach(0..<viewModel.columnCount) { columnIndex in
+                    if columnIndex < colorCode.count { // check if the current index is valid
+                        Circle()
+                            .foregroundColor(color[columnIndex])
+                            .overlay(Circle().stroke(Color.black, lineWidth: 2))
+                            .frame(width: 40, height: 40)
+                    }else{
+                        // Display a gray circle if there is no color in the array
+                        GrayCircleView()
                     }
                 }
             }
-        }
     }
 }
 
